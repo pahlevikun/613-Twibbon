337121ff47e6d7106d890cdb6e0c77947ce990d1
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-06-21 18:26:49 +0700

    First Commit - 21/07/18 18:26

---
+++ app/src/main/java/id/pahlevikun/twibbon613/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.pahlevikun.twibbon613
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
