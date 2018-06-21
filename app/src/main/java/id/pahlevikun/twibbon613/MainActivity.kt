3a606b8aa0d2a57d7f077d529faba802951d216a
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-06-22 00:59:06 +0700

    Add new version - 22/07/18 00:58

--- app/src/main/java/id/pahlevikun/twibbon613/MainActivity.kt
+++ app/src/main/java/id/pahlevikun/twibbon613/MainActivity.kt
@@ -80,6 +80,7 @@ class MainActivity : AppCompatActivity() {
                 output = FileOutputStream("$dirPath$fileName")
                 viewToBitmap(frameLayout).compress(Bitmap.CompressFormat.PNG, 90, output)
                 output!!.close()
+                Snackbar.make(coordinatorLayout, getString(R.string.snackbar_main_success), Snackbar.LENGTH_SHORT).show()
             } else {
                 Snackbar.make(coordinatorLayout, getString(R.string.snackbar_main_pleasecapture), Snackbar.LENGTH_SHORT).show()
             }
@@ -108,7 +109,6 @@ class MainActivity : AppCompatActivity() {
             if (image != null) {
                 isImageSelected = true
                 Glide.with(this).load(image.path).into(imageViewUser)
-                Snackbar.make(coordinatorLayout, getString(R.string.snackbar_main_success), Snackbar.LENGTH_SHORT).show()
             } else {
                 Snackbar.make(coordinatorLayout, getString(R.string.snackbar_main_pleasecapture), Snackbar.LENGTH_SHORT).show()
             }
